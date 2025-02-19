44d50b26b6e301672754502a99c4b9b95dca345d
truongsonkmhd <truongsonkmhd@gmail.com>
2025-02-19 07:34:29 +0700

    dang_nhap

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+    }
+
+    public static void changLNF(String nameLNF) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if (nameLNF.equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+        }
+    }
+}
